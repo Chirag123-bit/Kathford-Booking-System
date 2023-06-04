eb77b305bc90d3e8c7e22500d32f4137b29b1b09
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-06-04 19:45:11 +0545

    Details Page Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
+import Details from './components/Home/Details';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/acc' element={<Details />} />
 
         <Route path='/login' element={<h1>Login page</h1>} />
         <Route path='/register' element={<h1>Register page</h1>} />
