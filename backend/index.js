1e3b1117175f75538a7c2a3e19bb7d314ce2577f
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-06-13 09:21:26 +0545

    Backend Configuration

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+require("./database/connection")
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server has started in port ${process.env.PORT}`)
+})
+
+
+
