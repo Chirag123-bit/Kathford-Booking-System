0b2a2cc82a03c03b13a55fc24884c9e1e04fedc7
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-06-17 13:10:59 +0545

    Morgan:

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 
 require("dotenv").config()
 require("./database/connection")
 
 app.use(express.json())
+app.use(morgan("dev"))
 
 const userRoute = require("./routes/UserRoute")
 app.use("/api/user", userRoute)
