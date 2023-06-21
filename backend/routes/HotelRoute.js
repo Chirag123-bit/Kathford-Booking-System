ebe15402268af39f9f4839ef2c642b34bdb60b09
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-06-21 10:42:56 +0545

    Update and Get Hotels

--- backend/routes/HotelRoute.js
+++ backend/routes/HotelRoute.js
@@ -1,10 +1,13 @@
-const { addHotel } = require("../controller/HotelController")
+const { addHotel, getAllHotels, getHotel, updateHotel } = require("../controller/HotelController")
 const { isAuthenticated, authorizedRole } = require("../middleware/auth")
 
 
 const router = require("express").Router()
 
 router.post("/add", isAuthenticated, addHotel)
+router.get("/all", getAllHotels)
+router.get("/:id", getHotel)
+router.patch("/", isAuthenticated, updateHotel)
 
 
 module.exports = router
