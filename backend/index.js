5512e3059097099fd1185449b39cff7a07d027dc
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-06-22 10:22:13 +0545

    Backend partially completed

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,12 @@ app.use(express.json())
 const userRoute = require("./routes/UserRoute")
 const hotelRoute = require("./routes/HotelRoute")
 const accomodationRoute = require("./routes/AccomodationRoute")
+const bookingRoute = require("./routes/BookingRoute")
 
 app.use("/api/user", userRoute)
 app.use("/api/hotel", hotelRoute)
 app.use("/api/acc", accomodationRoute)
+app.use("/api/book", bookingRoute)
 
 
 app.listen(process.env.PORT, () => {
